317274ef6406b90d6a7af5aa463aad34fa8d7aa9
Borys Majewski <borys.t.majewski@gmail.com>
2024-11-06 00:41:13 +0100

    Created answer to task 2 using prompt injection. Housekeeping in task 1. Added new relevant files to gitignore

--- task_1_01_connect_get_captcha.py
+++ task_1_01_connect_get_captcha.py
@@ -9,13 +9,10 @@ from bs4 import BeautifulSoup
 from api_tasks.basic_poligon_u import load_from_json
 from api_tasks.get_html_fragment import get_strings_re
 from api_tasks.website_interactions import scrape_site
-from api_tasks.basic_open_ai_calls import opai_call, gather_calls
+from api_tasks.basic_open_ai_calls import gather_calls
 from api_tasks.ready_logins import login_to_website
 
-
-
-
-def get_this_secret_info_from_website(username, password, login_url, target_url,save_website_locally = True):
+def get_this_secret_info_from_website(username, password, login_url, target_url,save_website_locally = True, max_retries = 5):
     session = requests.Session()
     
     website_html = scrape_site(session, login_url)
@@ -24,13 +21,28 @@ def get_this_secret_info_from_website(username, password, login_url, target_url,
     # Call openai for help :/ 
     client = OpenAI(api_key=api_key)
     my_questions = [
-      {'user':right_question,'system':'Question will be in polish. Reply only with single number pertaining to question. Answer will be a positive integer.', 'assistant': 'Reply with number only.'}
+      { 'user':right_question
+       ,'system':'Question will be in polish. Reply only with single number pertaining to question. Answer will be a positive integer. Only in case it is not possible to return positive number as answer to question, return 0.'
+       ,'assistant': 'Reply with number only.'
+       }
     ]
-
-    my_answers = asyncio.run(gather_calls(my_questions,client = client))
-
+    
+    retry_c = 0
+    while retry_c < max_retries:
+        retry_c = retry_c + 1
+        my_answers = asyncio.run(gather_calls(my_questions,client = client))
+        try:
+            if int(my_answers[0]) != 0 and my_answers is not None:
+                break
+            else:
+                pass
+        except:
+            pass
+    
     # should be username, password, answer (for captcha)
     # Prepare login data
+    print(f"Received answer: {my_answers[0]}")
+
     success, info = login_to_website(target_url, username, password, my_answers[0])
     
     if save_website_locally:
